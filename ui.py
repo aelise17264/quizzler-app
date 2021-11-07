a57d9b51fe889534c36f7b69156868c303a8cce5
Anna <aelise17264@gmail.com>
2021-11-06 19:09:28 -0600

    updated true & false buttons so that the user's answer is checked against the question's answer to determine if it was right or wrong, the canvas changes color to alert the user to the results and the score increments with every correct answer

--- ui.py
+++ ui.py
@@ -25,11 +25,11 @@ class QuizInterface:
         self.scoreboard.grid(column=0, row=1, columnspan=2, pady=50)
 
         true = PhotoImage(file="images/true.png")
-        self.green = Button(image=true, padx=20, pady=20, highlightthickness=0)
+        self.green = Button(image=true, padx=20, pady=20, highlightthickness=0, command=self.answer_true)
         self.green.grid(column=0, row=2)
 
         false = PhotoImage(file="images/false.png")
-        self.red = Button(image=false, highlightthickness=0)
+        self.red = Button(image=false, highlightthickness=0, command=self.answer_false)
         self.red.grid(column=1, row=2)
 
         self.get_next_question()
@@ -38,5 +38,24 @@ class QuizInterface:
 
     def get_next_question(self):
         # tap into quiz_brain & get next question
+        self.scoreboard.config(bg="#FFFFFF")
+        self.score.config(text=f"Score: {self.quiz.score}")
         q_text = self.quiz.next_question()
         self.scoreboard.itemconfig(self.question_text, text=q_text)
+
+    def answer_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.player_feedback(is_right)
+
+    def answer_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.player_feedback(is_right)
+
+    def player_feedback(self, is_right):
+        if is_right:
+            self.scoreboard.config(bg="green")
+        else:
+            self.scoreboard.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
